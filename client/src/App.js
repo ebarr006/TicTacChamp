7feb283bf40f533298a5f957c84d3917a3a2cfa8
Emilio <ebarr006@ucr.edu>
2019-05-03 00:11:54 -0700

    setting up react front end / express backend

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      apiRes: ""
+    };
+  }
+
+  callAPI() {
+    fetch('http://localhost:9000/testAPI')
+    .then(res => res.text())
+    .then(res => this.setState({apiRes: res}))
+    .catch(err => err);
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">{this.state.apiRes}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
